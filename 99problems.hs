37826a7cee7817c1146f214b5a51ae7faeaa1dfb
Chris Coffey <cmcof273@gmail.com>
2015-11-16 07:24:53 -0500

    run length encoding

--- 99problems.hs
+++ 99problems.hs
@@ -31,4 +31,17 @@ pack (l:ls) = reverse (rec [] [l] ls) where
     rec acc as (b:bs) = if ((head as) == b) then rec acc (b:as) bs else rec (as:acc) [b] bs
 --10
 runLengthEncode ls = map (\xs -> (size xs, head xs)) (pack ls)
+--11
+data RLE a = Mult Int a | Sing a deriving (Show)
+encodeModified :: Eq a =>  [a] -> [RLE a]
+encodeModified ls = map (\(i, e) -> if(i == 1) then Sing e else Mult i e) $ runLengthEncode ls
+--12
+decodeRLE ls = concatMap (\l -> f l) ls where
+    f (Sing x) = [x] 
+    f (Mult i x) = replicate i x
+--13
+directRLE:: Eq a => [a] -> [RLE a]
+directRLE (x:xs) = reverse $ foldl f ([], x, 1) xs where
+    f (acc, s, n) i = if s == i then (acc, s, n +1)
+                      else if n == 1 then ((Sing s):acc, i, 1) else ((Mult i s):acc, i, 1)
 
