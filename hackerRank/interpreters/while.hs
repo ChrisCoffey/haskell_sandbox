24a66a52573249f20ef99923af6793ac5991a8ad
Chris Coffey <cmcof273@gmail.com>
2016-02-17 07:10:05 -0500

    only a couple of arithmetic operators

--- hackerRank/interpreters/while.hs
+++ hackerRank/interpreters/while.hs
@@ -2,20 +2,28 @@ import Control.Monad
 import Data.Functor
 import qualified Data.Map           as M
 
-type Var    = String
-type N      = Integer
+newtype Var = Var String
+newtype N   = N Integer
 data Oα     = Plus | Minus | Mult | Div
 data Oβ     = And | Or
 data Oρ     = Gt | Lt
 data AExp   = ExpAV Var | ExpAN N | ExpAO Oα AExp Oα | ExpAP AExp
-data BExp   = T | F | BExpB BExp Oβ BExp | BExpR BExp Oρ BExp | BExpP BExp 
+data BExp   = T | F | BExpB BExp Oβ BExp | BExpR AExp Oρ AExp | BExpP BExp 
 data Stmt   = ASSIGN Var AExp | SEQ Stmt Stmt | IF BExp Stmt Stmt | WHILE BExp Stmt
 
-type PState = M.Map String N
-type Prog   = M.Map Int Stmt -- not thrilled with representing a program as a simple sequence of statements
+newtype PState = PState (M.Map String N)
+newtype Prog   = Prog (M.Map Int Stmt) -- not thrilled with representing a program as a simple sequence of statements
+
+--need to implement the interpreter for the language above
+lookupVar (Var s) (PState m) = M.lookup s m
+extractN (N i) = i
+(N l) +: (N r) = N (l + r)
+(N l) -: (N r) = N (l - r)
+(N l) *: (N r) = N (l * r)
+(N l) /: (N r) = N (l `div` r)
+
 
 --need to implement a recursive descent parser
---need to implement the interpreter for the language above
 
 main = do
     ls <- getContents
