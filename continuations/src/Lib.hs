810912321684f75a6ac83a4d41209412e1f3556e
Chris Coffey <cmcof273@gmail.com>
2019-02-20 07:28:14 -0500

    Continuation Instances

--- continuations/src/Lib.hs
+++ continuations/src/Lib.hs
@@ -1,23 +1,24 @@
 module Lib where
 
 -- Continuations are simply functions that take additional functions rather than return a value. The following is a CPS version of `if/else`
-newtype CPS a r = CPS {runCPS :: ((a -> r) -> r)}
+newtype CPS r a  = CPS {runCPS :: ((a -> r) -> r)}
+
 
 add' :: Num a =>
     a
     -> a
-    -> CPS a r
+    -> CPS r a
 add' a b = CPS $ \next -> next (a+b)
 
 square' :: Num a =>
     a
-    -> CPS a r
+    -> CPS r a
 square' a = CPS $ \next -> next (a*a)
 
 pythagoras' :: Num a =>
     a
     -> a
-    -> CPS a r
+    -> CPS r a
 pythagoras' a b = CPS $ \terminator ->
     runCPS (square' a) $ \a2 ->
     runCPS (square' b) $ \b2 ->
@@ -27,21 +28,21 @@ pythagoras' a b = CPS $ \terminator ->
 
 plusTwenty :: Num a =>
     a
-    -> CPS a res
+    -> CPS res a
 plusTwenty x = CPS $ \next -> next $ x+20
 
 terniary ::
     Bool
-    -> CPS a r
-    -> CPS a r
-    -> CPS a r
+    -> CPS r a
+    -> CPS r a
+    -> CPS r a
 terniary cond yes no
     | cond = yes
     | otherwise = no
 
 example :: (Num a, Ord a) =>
     a
-    -> CPS a r
+    -> CPS r a
 example n =
     terniary (n < 50)
              (pythagoras' 19 n)
@@ -57,25 +58,25 @@ example n =
 -- c
 --
 try ::
-    (CPS a r -> CPS a r -> CPS a r) -- ^ try body
-    -> (CPS a r -> CPS a r -> CPS a r) -- ^ catch body
-    -> CPS a r -- ^ outer normal
-    -> CPS a r -- ^ outer error
-    -> CPS a r
+    (CPS r a -> CPS r a -> CPS r a) -- ^ try body
+    -> (CPS r a -> CPS r a -> CPS r a) -- ^ catch body
+    -> CPS r a -- ^ outer normal
+    -> CPS r a -- ^ outer error
+    -> CPS r a
 try tBody cBody outerSucc outerErr = CPS $ \next ->
     runCPS (tBody outerSucc $ cBody outerSucc outerErr) $ next
 
 
 -- Given two CPS, call the second one
 throw ::
-    CPS a r -- ^ normal path. This is ignored
-    -> CPS a r -- ^ Error path
-    -> CPS a r
+    CPS r a -- ^ normal path. This is ignored
+    -> CPS r a -- ^ Error path
+    -> CPS r a
 throw = flip const
 
 tryCatchExample :: (Num a, Ord a) =>
     a
-    -> CPS a r
+    -> CPS r a
 tryCatchExample n =
     try
         (\ happy sad -> terniary (n < 10)
@@ -92,5 +93,32 @@ tryCatchExample n =
         topLevelError = CPS $ error "Hit an error!"
 
 
+-- Build up a function that returns ((b -> r) -> r). Do this by combining the two CPS calls together.
 
+chainCPS ::
+    ((a -> r) -> r)
+    -> (a -> ((b -> r) -> r))
+    -> ((b -> r) -> r)
+chainCPS cont next =
+    \a -> cont $ \x -> next x $ a
+    -- a ^ above is a (b -> r), while x is an 'a'. This is clear because `cont` takes an `a -> r`, so the
+    -- lambda passed to it has an `a` as its first argument. Similarly, next first takes an `a`, then a
+    -- continuation. That continuation is the argument `a`. So the entire function returns an `r` after all
+    -- the composition is completed.
 
+instance Functor (CPS r) where
+    fmap f (CPS cont) =
+        CPS $ \next -> cont $ \a -> next $ f a
+
+instance Applicative (CPS r) where
+    pure a = CPS $ \next -> next a
+    -- f :: (((a -> b) -> r) -> r)
+    -- cont :: ((a -> r) -> r)
+    -- <*> :: (b -> r) -> r
+    (CPS f) <*> (CPS cont) =
+        CPS $ \next ->
+            f $ \abr -> cont $ next . abr
+
+instance Monad (CPS r) where
+    (CPS cont) >>= next =
+        CPS $ \b -> cont $ \a -> runCPS (next a) b
